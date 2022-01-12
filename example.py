4647dec2ad82ab25170fbfd7aac17bdc5c6dfd66
denzelbrown <denzelbrown@icloud.com>
2022-01-11 22:18:52 -0600

    Delete example.py

--- example.py
+++
@@ -1,8 +0,0 @@
-import datetime
-from datetime import datetime
-
-date1 = "2022-01-07 14:53:53.029980"
-date2 = "2022-01-08T15:57:23.616Z"
-#print(datetime.strptime(date2, "%Y-%m-%dT%H:%M%S.%fZ"))
-date2 = date2.replace("T", " ")
-print(date2[:-1] + "000" + date2[-1])
